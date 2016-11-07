2a9220ee54301a868b1e8f314241ee840097f189
yharasym <garasym@gmail.com>
2016-11-07 11:18:35 +0200

    Bug Fixed

--- src/Appl.java
+++ src/Appl.java
@@ -1,11 +1,12 @@
 public class Appl {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		String text = "Hello World";
 		Box box = new Box();
 		box.set(text);
-		Integer i = (Integer) box.get();
-		System.out.println(i);
+		//Integer i = (Integer) box.get();
+		//System.out.println(i);
 	}
 	
 }
