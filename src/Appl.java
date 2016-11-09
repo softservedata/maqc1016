86a0cbf31e2c22fcd61e055d59c290b1cb1c8064
dshevch <daria.semenuyk@gmail.com>
2016-11-09 22:38:55 +0200

    Fixed name spelling.

--- src/Appl.java
+++ src/Appl.java
@@ -2,7 +2,7 @@ public class Appl {
 
 	public static void main(String[] args) {
 		System.out.println("Start me 1");
-		String text = "Hello World from Draiia Shevchuk";
+		String text = "Hello World from Dariia Shevchuk";
 		Box box = new Box();
 		box.set(text);
 //		Integer i = (Integer) box.get();
