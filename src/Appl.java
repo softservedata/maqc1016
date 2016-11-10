8f0089e7ae14047c8e999cdb293ce1e5a4b433c0
Yevgeniy Kundryukov <forgedone@gmail.com>
2016-11-10 21:52:52 +0200

    commitment

--- src/Appl.java
+++ src/Appl.java
@@ -1,21 +1,19 @@
 public class Appl {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
+
 		String text = "Hello World";
 		Box box = new Box();
 		box.set(text);
-		Integer i = (Integer) box.get();
-		System.out.println(i);
-=======
-		System.out.println("Start me 1");
-		String text = "Hello World from Dariia Shevchuk";
-		Box box = new Box();
-		box.set(text);
-//		Integer i = (Integer) box.get();
+		//Integer i = (Integer) box.get();
 		System.out.println(text);
-		System.out.println("Softserveinc");
->>>>>>> 44200e4f7cb3eac7e5f4500b2c19073949f874fb
+
+
+		System.out.println("*/*/*/*/*/*");
+		JustForFun jff = new JustForFun();
+		System.out.println("Something will happen(sum 1+2)=" + jff.sum(1,2));
+
 	}
 	
 }
---
+++ src/JustForFun.java
@@ -0,0 +1,9 @@
+/**
+ * Created by forgedone on 10.11.2016.
+ */
+class JustForFun {
+
+    public int sum(int first, int second){
+        return (first+second);
+    }
+}
