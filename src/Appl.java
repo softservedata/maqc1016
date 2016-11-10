13ec161c8e71e36d36ea44e7ba5f9569e0d6a47c
dshevch <daria.semenuyk@gmail.com>
2016-11-10 22:00:12 +0200

    Added new strings with a few messages.

--- src/Appl.java
+++ src/Appl.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Appl {
 
 	public static void main(String[] args) {
@@ -8,6 +10,20 @@ public class Appl {
 //		Integer i = (Integer) box.get();
 		System.out.println(text);
 		System.out.println("Softserveinc");
+
+		System.out.println("November 11");
+
+
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		System.out.println("Please, enter first number");
+		int FirstNumber = scanner.nextInt();
+		System.out.println("Please, enter second number");
+		int SecondNumber = scanner.nextInt();
+		System.out.println("The result is next");
+		int Result = FirstNumber * SecondNumber;
+		System.out.println(Result);
 	}
+
 	
 }
