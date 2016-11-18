98bfd3b6910f22cdd031113af68a12a21ac5e991
yharasym <garasym@gmail.com>
2016-11-18 09:42:37 +0200

    Parse first column

--- src/com/string/edu/RegexTest.java
+++ src/com/string/edu/RegexTest.java
@@ -18,9 +18,17 @@ public class RegexTest {
 		//String pattern = "<(\\w+)>[\\w .,]+</\\1>"; // Include all Tags
 		//String text = "<p><b>Beginning with bold text</b> next, text body,<i>italic text</i> end of text.</p>";
 		//
-		String pattern = "\\w+(\\.\\w+)*@(\\w+\\.)+\\w{2,}";
+		//String pattern = "\\w+(\\.\\w+)*@(\\w+\\.)+\\w{2,}";
 		//String text = "a.b_c.d@i.ua";
-		String text = "a.b_c.d@i.ua aa_@gmail.com;bb@ukr.net";
+		//String text = "a.b_c.d@i.ua aa_@gmail.com;bb@ukr.net";
+		//
+		String pattern = "<tr>[^<]*<td>[^<]*</td>";
+		String text = "<table>" 
+				+ "<tr>     <th>Month</th>    <th>Count of days</th>   </tr>"
+				+ "<tr>     <td>January</td>     <td>30</td>    </tr>"
+				+ "<tr>      <td>February</td>   <td>28</td>   </tr>"
+				+ "<tr>      <td>March</td>       <td>31</td>    </tr>"
+				+ "</table>";
 		//
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher(text);
@@ -35,7 +43,8 @@ public class RegexTest {
 			//result.add(text.substring(m.start(), m.end()));
 			//System.out.print(text.substring(m.start(), m.end()) + "*");
 			//System.out.println(text.substring(m.start()+1, m.end()-1).trim()); // Tag content 
-			System.out.println(text.substring(m.start(), m.end())); // Tag content
+			//System.out.println(text.substring(m.start(), m.end())); // Tag content
+			System.out.println(text.substring(m.start()+4, m.end()).trim());
 		}
 		//System.out.println("\nResult = " + result);
 	}
