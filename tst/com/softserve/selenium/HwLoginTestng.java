9d827b60fc798beb8318fe176e8e6c5b38107536
vromank <vasiliy.romanko@gmail.com>
2016-11-22 16:41:01 +0200

    login test homework

---
+++ tst/com/softserve/selenium/HwLoginTestng.java
@@ -0,0 +1,28 @@
+package com.softserve.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HwLoginTestng {
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/Users/vromank/Documents/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://registrator.herokuapp.com/login/");
+		driver.findElement(By.id("login")).clear();
+		driver.findElement(By.id("login")).sendKeys("test");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("qwerty");
+		driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+		System.out.println("Page title is: " + driver.getTitle());
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		(new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.textToBe(By.xpath("//div[@class='btn-group']/button[1]"), "test"));
+		driver.quit();
+	}
+}
