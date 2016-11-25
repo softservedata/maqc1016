0447917ea5ceb39bd520731ffb9b1792793647b7
yharasym <garasym@gmail.com>
2016-11-25 10:32:29 +0200

    add Selenium 3.0

---
+++ src/com/softserve/edu/Browsers.java
@@ -0,0 +1,60 @@
+package com.softserve.edu;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.testng.annotations.Test;
+
+public class Browsers {
+
+	// @Test // 2.53.1
+	public void testFirefox1() throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		//
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Cheese!");
+		element.submit();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	//@Test // 3.0.1
+	public void testFirefox12() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "./lib/geckodriver.exe");
+		// "C:\Users\yharasym.TRAINING\workspace\maqc1016\lib\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		 //
+		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		 driver.get("http://www.google.com");
+		 WebElement element = driver.findElement(By.name("q"));
+		 element.sendKeys("Cheese!");
+		 element.submit();
+		 Thread.sleep(2000);
+		 driver.quit();
+	}
+
+	@Test
+    public void testFirefox2() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "./lib/geckodriver.exe");
+        ProfilesIni profileIni = new ProfilesIni();
+        FirefoxProfile profile = profileIni.getProfile("default");
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setPreference("app.update.enabled", false);
+        WebDriver driver = new FirefoxDriver(profile);
+        //
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!");
+        element.submit();
+        Thread.sleep(2000);
+        //driver.quit();
+    }
+}
