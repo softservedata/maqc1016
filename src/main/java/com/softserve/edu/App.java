10a497d879440db55e9a3482a7f1298ac3d326b5
yharasym <garasym@gmail.com>
2016-12-05 10:59:31 +0200

    Maven Template project

---
+++ src/main/java/com/softserve/edu/App.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+public class App 
+{
+
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,13 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+	
+}
